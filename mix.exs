bc7a2f1ce1b69579bb0a8c33a5ed502f60c273c3
Alan Rice <mail@alanrice.me>
2017-11-27 18:10:00 -0500

    add deps

--- mix.exs
+++ mix.exs
@@ -22,6 +22,11 @@ defmodule Tunk.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+		{:plug_cowboy2, github: "voicelayer/plug_cowboy2"},
+		{:ranch, github: "ninenines/ranch", ref: "1.3.0", override: true, manager: :rebar3},
+		{:cowlib, github: "ninenines/cowlib", ref: "master", override: true, manager: :rebar3},
+		{:plug, "~> 1.3.0"},
+	{:cowboy, github: "ninenines/cowboy", ref: "2.0.0-pre.7", override: true, manager: :rebar3},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
