0d1fa7a32f1c38da33489700dfb1b754be38b4f3
Alan Rice <mail@alanrice.me>
2017-11-27 18:16:27 -0500

    hello world

--- lib/tunk.ex
+++ lib/tunk.ex
@@ -1,4 +1,13 @@
 defmodule Tunk do
+	use Plug.Router
+
+	plug :match
+	plug :dispatch
+
+	get "/hello" do
+		Plug.Conn.send_resp(conn, 200, "ok")		
+	end
+
   @moduledoc """
   Documentation for Tunk.
   """
