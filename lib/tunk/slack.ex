fa0f66421a08646f8019a0da6c5ebeca882b98d0
Dax Ravi <d@ironbay.co>
2019-02-26 10:34:43 -0500

    Remove username override

--- lib/tunk/slack.ex
+++ lib/tunk/slack.ex
@@ -1,53 +1,58 @@
 defmodule Tunk.Slack do
-	alias Slack.Web.Chat, as: Slack
-	alias Tunk.Config
-	
-	def send(info) do
-		case Enum.member?(["pending", "success", "failure"], info.status) do 
-			true -> format(info)
-			false -> :noop
-		end 
-	end 
+  alias Slack.Web.Chat, as: Slack
+  alias Tunk.Config
 
-	def format(info) do 
-		context = "http://" <> info.context
+  def send(info) do
+    case Enum.member?(["pending", "success", "failure"], info.status) do
+      true -> format(info)
+      false -> :noop
+    end
+  end
 
-		branch_url = case info.branch do
-			"master" -> "https://github.com/" <> info.owner <> "/" <> info.repo
-			_ -> "https://github.com/" <> info.owner <> "/" <> info.repo <> "/tree/" <> info.branch
-		end
+  def format(info) do
+    context = "http://" <> info.context
 
-		branch_display = "<#{branch_url}|#{info.branch}>"
+    branch_url =
+      case info.branch do
+        "master" -> "https://github.com/" <> info.owner <> "/" <> info.repo
+        _ -> "https://github.com/" <> info.owner <> "/" <> info.repo <> "/tree/" <> info.branch
+      end
 
-		attachments = [
-			%{
-				"color": status_color(info.status), 
-				"text": "*Build Status*: #{String.capitalize(info.status)}\n*<#{info.target_url}|Build details>*\n*Branch*: #{branch_display}\n*Repo*: #{info.repo}\n*Images*: #{readable_list(info.images)}\n*<#{context}|Container registry>*", 
-				"mrkdwn_in": ["text"]
-			}
-		] |> Poison.encode!
-		
-		Config.tunk_slack_channel() 
-		|> Slack.post_message("", 
-			%{
-				token: Config.tunk_slack_token(),
-				username: "Tunk", 
-				icon_emoji: ":chart_with_upwards_trend:",
-				attachments: [attachments] 
-			}
-		) 
-	end
-	
-	def status_color(status) do
-		case status do
-			"pending" -> "#FFA100"
-			"success" -> "#006F13"
-			"failure" -> "#DC2B30"
-		end
-	end
+    branch_display = "<#{branch_url}|#{info.branch}>"
 
-	def readable_list(images) do
-		images 
-		|> List.foldl("", fn(x, acc) -> acc <> x <> "\n" end)
-	end
+    attachments =
+      [
+        %{
+          color: status_color(info.status),
+          text:
+            "*Build Status*: #{String.capitalize(info.status)}\n*<#{info.target_url}|Build details>*\n*Branch*: #{
+              branch_display
+            }\n*Repo*: #{info.repo}\n*Images*: #{readable_list(info.images)}\n*<#{context}|Container registry>*",
+          mrkdwn_in: ["text"]
+        }
+      ]
+      |> Poison.encode!()
+
+    Config.tunk_slack_channel()
+    |> Slack.post_message(
+      "",
+      %{
+        token: Config.tunk_slack_token(),
+        attachments: [attachments]
+      }
+    )
+  end
+
+  def status_color(status) do
+    case status do
+      "pending" -> "#FFA100"
+      "success" -> "#006F13"
+      "failure" -> "#DC2B30"
+    end
+  end
+
+  def readable_list(images) do
+    images
+    |> List.foldl("", fn x, acc -> acc <> x <> "\n" end)
+  end
 end
